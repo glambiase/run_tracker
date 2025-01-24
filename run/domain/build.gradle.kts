17d374b6dfd3269e7f2f8e688265205dd4dd81b2
Giampaolo Lambiase <giampaololambiase@gmail.com>
2025-01-24 12:05:10 +0100

    set-up version catalog

--- run/domain/build.gradle.kts
+++ run/domain/build.gradle.kts
@@ -1,9 +1,8 @@
 plugins {
     id("java-library")
-    alias(libs.plugins.jetbrainsKotlinJvm)
+    alias(libs.plugins.org.jetbrains.kotlin.jvm)
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+dependencies {
+    implementation(libs.kotlinx.coroutines.core)
 }
