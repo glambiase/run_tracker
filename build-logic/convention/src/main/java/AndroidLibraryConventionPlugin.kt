dd19228f6c45db4594a187d048855e24e27d4fce
Giampaolo Lambiase <giampaololambiase@gmail.com>
2025-03-30 15:03:02 +0200

    added android_library convention plugin

---
+++ build-logic/convention/src/main/java/AndroidLibraryConventionPlugin.kt
@@ -0,0 +1,35 @@
+import com.android.build.api.dsl.LibraryExtension
+import com.glambiase.build_logic.convention.ExtensionType
+import com.glambiase.build_logic.convention.configureBuildTypes
+import com.glambiase.build_logic.convention.configureKotlinAndroid
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.kotlin
+
+class AndroidLibraryConventionPlugin : Plugin<Project> {
+
+    override fun apply(target: Project) {
+        target.run {
+            pluginManager.run {
+                apply("com.android.library")
+                apply("org.jetbrains.kotlin.android")
+            }
+
+            extensions.configure<LibraryExtension> {
+                configureKotlinAndroid(commonExtension = this)
+                configureBuildTypes(commonExtension = this, extensionType = ExtensionType.LIBRARY)
+
+                defaultConfig {
+                    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+                    consumerProguardFiles("consumer-rules.pro")
+                }
+            }
+
+            dependencies {
+                "testImplementation"(kotlin("test"))
+            }
+        }
+    }
+}
