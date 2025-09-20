feb6fda592d5624299d467e03e7891d1c6462123
Giampaolo Lambiase <giampaololambiase@gmail.com>
2025-09-20 21:26:21 +0200

    added run_summary composable

---
+++ run/presentation/src/main/java/com/glambiase/run/presentation/run_overview/composables/RunSummary.kt
@@ -0,0 +1,322 @@
+@file:OptIn(ExperimentalLayoutApi::class, ExperimentalFoundationApi::class)
+
+package com.glambiase.run.presentation.run_overview.composables
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.SubcomposeAsyncImage
+import com.glambiase.core.domain.location.Location
+import com.glambiase.core.domain.run.Run
+import com.glambiase.core.presentation.designsystem.CalendarIcon
+import com.glambiase.core.presentation.designsystem.RunOutlinedIcon
+import com.glambiase.run.presentation.R
+import com.glambiase.core.presentation.designsystem.RunTrackerTheme
+import com.glambiase.run.presentation.run_overview.mapper.toRunUI
+import com.glambiase.run.presentation.run_overview.model.RunDataUi
+import com.glambiase.run.presentation.run_overview.model.RunUi
+import java.time.ZonedDateTime
+import kotlin.math.max
+import kotlin.time.Duration.Companion.minutes
+import kotlin.time.Duration.Companion.seconds
+
+@Composable
+fun RunSummary(
+    runUi: RunUi,
+    onDeleteClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    var showDropDown by rememberSaveable { mutableStateOf(false) }
+
+    Box {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = modifier
+                .clip(RoundedCornerShape(16.dp))
+                .background(color = MaterialTheme.colorScheme.surface)
+                .combinedClickable(
+                    onClick = {},
+                    onLongClick = { showDropDown = true }
+                )
+                .verticalScroll(rememberScrollState())
+                .padding(16.dp)
+        ) {
+            MapImage(imageUrl = runUi.mapPictureUrl)
+            HorizontalDivider(color = MaterialTheme.colorScheme.onSurfaceVariant.copy(alpha = 0.4f))
+            RunningTimeSection(duration = runUi.duration)
+            HorizontalDivider(color = MaterialTheme.colorScheme.onSurfaceVariant.copy(alpha = 0.4f))
+            RunningDateSection(dateTime = runUi.dateTime)
+            HorizontalDivider(color = MaterialTheme.colorScheme.onSurfaceVariant.copy(alpha = 0.4f))
+            DataGrid(runUi = runUi)
+        }
+        DropdownMenu(
+            expanded = showDropDown,
+            onDismissRequest = { showDropDown = false }
+        ) {
+            DropdownMenuItem(
+                text = {
+                    Text(
+                        text = stringResource(id = R.string.delete)
+                    )
+                },
+                onClick = {
+                    showDropDown = false
+                    onDeleteClick()
+                }
+            )
+        }
+    }
+}
+
+@Composable
+private fun MapImage(
+    imageUrl: String?,
+    modifier: Modifier = Modifier
+) {
+    SubcomposeAsyncImage(
+        model = imageUrl,
+        contentDescription = stringResource(id = R.string.run_map),
+        modifier = modifier
+            .fillMaxWidth()
+            .aspectRatio(ratio = 16 / 9f)
+            .clip(RoundedCornerShape(16.dp)),
+        loading = {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator(
+                    modifier = Modifier.size(20.dp),
+                    strokeWidth = 2.dp,
+                    color = MaterialTheme.colorScheme.onSurface
+                )
+            }
+        },
+        error = {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(color = MaterialTheme.colorScheme.errorContainer),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = stringResource(R.string.image_loading_error),
+                    color = MaterialTheme.colorScheme.error
+                )
+            }
+        }
+    )
+}
+
+@Composable
+private fun RunningTimeSection(
+    duration: String,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+    ) {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .size(40.dp)
+                .clip(RoundedCornerShape(8.dp))
+                .background(MaterialTheme.colorScheme.primary.copy(alpha = 0.1f))
+                .border(
+                    width = 1.dp,
+                    color = MaterialTheme.colorScheme.primary,
+                    shape = RoundedCornerShape(8.dp)
+                )
+                .padding(4.dp)
+        ) {
+            Icon(
+                imageVector = RunOutlinedIcon,
+                contentDescription = null,
+                tint = MaterialTheme.colorScheme.primary
+            )
+        }
+        Spacer(modifier = Modifier.width(16.dp))
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = stringResource(R.string.running_time),
+                color = MaterialTheme.colorScheme.onSurfaceVariant
+            )
+            Text(
+                text = duration,
+                color = MaterialTheme.colorScheme.onSurface
+            )
+        }
+    }
+}
+
+@Composable
+private fun RunningDateSection(
+    dateTime: String,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+    ) {
+        Icon(
+            imageVector = CalendarIcon,
+            contentDescription = null,
+            tint = MaterialTheme.colorScheme.onSurfaceVariant
+        )
+        Spacer(modifier = Modifier.width(16.dp))
+        Text(
+            text = dateTime,
+            color = MaterialTheme.colorScheme.onSurfaceVariant
+        )
+    }
+}
+
+@Composable
+private fun DataGrid(
+    runUi: RunUi,
+    modifier: Modifier = Modifier
+) {
+    val runDataUiList = listOf(
+        RunDataUi(
+            text = stringResource(id = R.string.distance),
+            value = runUi.distance
+        ),
+        RunDataUi(
+            text = stringResource(id = R.string.pace),
+            value = runUi.pace
+        ),
+        RunDataUi(
+            text = stringResource(id = R.string.avg_speed),
+            value = runUi.avgSpeed
+        ),
+        RunDataUi(
+            text = stringResource(id = R.string.max_speed),
+            value = runUi.maxSpeed
+        ),
+        RunDataUi(
+            text = stringResource(id = R.string.tot_elevation),
+            value = runUi.totalElevation
+        )
+    )
+
+    var maxCellWidth by remember { mutableIntStateOf(0) }
+    val maxCellWidthDp = with(LocalDensity.current) {
+        maxCellWidth.toDp()
+    }
+    FlowRow(
+        horizontalArrangement = Arrangement.Center,
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier.fillMaxWidth()
+    ) {
+        runDataUiList.forEach { runDataUi ->
+            DataGridCell(
+                runDataUi = runDataUi,
+                modifier = Modifier
+                    .defaultMinSize(minWidth = maxCellWidthDp)
+                    .onSizeChanged {
+                        maxCellWidth = max(maxCellWidth, it.width)
+                    }
+            )
+        }
+    }
+}
+
+@Composable
+private fun DataGridCell(
+    runDataUi: RunDataUi,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+            .padding(8.dp)
+            .border(
+                shape = RoundedCornerShape(16.dp),
+                width = 1.dp,
+                color = MaterialTheme.colorScheme.onSurfaceVariant
+            )
+            .padding(8.dp)
+    ) {
+        Text(
+            text = runDataUi.text,
+            color = MaterialTheme.colorScheme.onSurfaceVariant,
+            fontSize = 12.sp
+        )
+        Spacer(modifier = Modifier.height(4.dp))
+        Text(
+            text = runDataUi.value,
+            color = MaterialTheme.colorScheme.onSurface
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun RunListItemPreview() {
+    RunTrackerTheme {
+        RunSummary(
+            runUi = Run(
+                id = "101",
+                duration = 60.minutes + 45.seconds,
+                dateTimeUtc = ZonedDateTime.now(),
+                distanceMeters = 6729,
+                location = Location(lat = 0.0, long = 0.0),
+                maxSpeedKmH = 15.14126,
+                totalElevationMeters = 28,
+                mapPictureUrl = null
+            ).toRunUI(),
+            onDeleteClick = {}
+        )
+    }
+}
---
+++ run/presentation/src/main/java/com/glambiase/run/presentation/run_overview/model/RunDataUi.kt
@@ -0,0 +1,6 @@
+package com.glambiase.run.presentation.run_overview.model
+
+data class RunDataUi(
+    val text: String,
+    val value: String
+)
